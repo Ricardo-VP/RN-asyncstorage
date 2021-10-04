6efd93a28ebb271d465370315af3afb0537a5c4b
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-10-04 00:17:43 -0500

    Obteniendo el nombre del storage

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   TextInput,
   Button,
@@ -10,11 +10,30 @@ import {
 import AsyncStorage from '@react-native-community/async-storage';
 
 const App = () => {
+
   const [inputTexto, setInputTexto] = useState(''); // Estado inicial
+  const [nombreStorage, setNombreStorage] = useState(''); // Estado inicial
+
+  useEffect(() => {
+    obtenerDatosStorage();
+  }, [])
 
   const guardarDatos = async () => {
     try {
       await AsyncStorage.setItem('nombre', inputTexto); // Guardar datos
+      setNombreStorage(inputTexto); // Actualizar el estado
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const obtenerDatosStorage = async () => {
+    try {
+      const nombre = await AsyncStorage.getItem('nombre'); // Obtener datos
+      if (nombre !== null) {
+        setNombreStorage(nombre);
+        // console.log(nombre);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -23,6 +42,7 @@ const App = () => {
   return (
     <>
       <View style={styles.contenedor}>
+        <Text>Hola: {nombreStorage}</Text>
         <TextInput
           placeholder="Escribe tu nombre"
           style={styles.input}
