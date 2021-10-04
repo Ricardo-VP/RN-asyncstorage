b4f198169ca8119bdb0942cc177a56203792ac33
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-10-04 00:05:53 -0500

    Agregando textInput, boton guardar y eliminar

--- App.js
+++ App.js
@@ -12,13 +12,43 @@ const App = () => {
 
   return (
     <>
-
+      <View style={styles.contenedor}>
+        <TextInput style={styles.input}/>
+        <Button title="Guardar" color="#333"/>
+        <TouchableHighlight style={styles.btnEliminar}>
+          <Text style={styles.txtEliminar}>Eliminar Nombre &times;</Text>
+        </TouchableHighlight>
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-
+  contenedor : {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input : {
+    borderColor: '#666',
+    borderBottomWidth: 1,
+    width: 300,
+    height: 40,
+    marginBottom: 20,
+  },
+  btnEliminar : {
+    backgroundColor: 'red',
+    marginTop: 20,
+    padding: 10,
+  },
+  txtEliminar : {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    width: 300,
+  }
 });
 
 export default App;
