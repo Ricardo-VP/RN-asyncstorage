0f1f336e65c77799f4595a156513cff9dc6b4fe1
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-10-04 00:11:55 -0500

    Guardando nombre en el storage

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   TextInput,
   Button,
@@ -7,14 +7,28 @@ import {
   Text,
   View,
 } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const App = () => {
+  const [inputTexto, setInputTexto] = useState(''); // Estado inicial
+
+  const guardarDatos = async () => {
+    try {
+      await AsyncStorage.setItem('nombre', inputTexto); // Guardar datos
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
       <View style={styles.contenedor}>
-        <TextInput placeholder="Escribe tu nombre" style={styles.input}/>
-        <Button title="Guardar" color="#333"/>
+        <TextInput
+          placeholder="Escribe tu nombre"
+          style={styles.input}
+          onChangeText={texto => setInputTexto(texto)}
+        />
+        <Button title="Guardar" color="#333" onPress={() => guardarDatos()} />
         <TouchableHighlight style={styles.btnEliminar}>
           <Text style={styles.txtEliminar}>Eliminar Nombre &times;</Text>
         </TouchableHighlight>
@@ -24,31 +38,31 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  contenedor : {
+  contenedor: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input : {
+  input: {
     borderColor: '#666',
     borderBottomWidth: 1,
     width: 300,
     height: 40,
     marginBottom: 20,
   },
-  btnEliminar : {
+  btnEliminar: {
     backgroundColor: 'red',
     marginTop: 20,
     padding: 10,
   },
-  txtEliminar : {
+  txtEliminar: {
     color: '#fff',
     fontWeight: 'bold',
     textAlign: 'center',
     textTransform: 'uppercase',
     width: 300,
-  }
+  },
 });
 
 export default App;
