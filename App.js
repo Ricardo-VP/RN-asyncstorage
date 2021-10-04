c09799e7fa0673aac98756c47acd92ae92a99dd1
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-10-03 23:58:34 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  TextInput,
+  Button,
+  TouchableHighlight,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
